adc510c181a34693df85f08b4e777d82a436ef35
koechslin <killian.oechslin@insa-lyon.fr>
2021-09-30 10:35:27 -0400

    [dev] add colors captions in rendering

--- MansionCleaner/src/renderer.py
+++ MansionCleaner/src/renderer.py
@@ -42,6 +42,8 @@ class Renderer:
 
         self.__drawRobot(startX, startY, state.getRobot())
 
+        self.__drawColorsCaptions(mansionSize)
+
         turtle.update()
 
     def __drawSquare(self, color):
@@ -68,3 +70,42 @@ class Renderer:
         turtle.circle(self.__squareSize / 4)
         turtle.up()
         turtle.end_fill()
+
+    def __drawColorsCaptions(self, mansionSize):
+        textOffsetX = 20 # offset to write text a little bit below each square
+        textOffsetY = 0 # offset to center text with each square
+        startX = int(mansionSize / 2) * self.__squareSize - self.__squareSize * 6
+        startY = -int(mansionSize / 2) * self.__squareSize
+
+        turtle.pencolor("black")
+
+        # 'Empty' caption
+        textOffsetY = self.__squareSize - 7 * 8 # squareSize - (3 * textLength / 2) * fontSize
+        turtle.goto(startY, startX)
+        self.__drawSquare("white")
+        turtle.goto(startY + textOffsetY, startX - textOffsetX)
+        turtle.write("Empty")
+
+        # 'Dirt' caption
+        textOffsetY = self.__squareSize - 6 * 8 # squareSize - (3 * textLength / 2) * fontSize
+        startY += 4 * self.__squareSize / 3
+        turtle.goto(startY, startX)
+        self.__drawSquare("red")
+        turtle.goto(startY + textOffsetY, startX - textOffsetX)
+        turtle.write("Dirt")
+
+        # 'Jewel' caption
+        textOffsetY = self.__squareSize - 7 * 8 # squareSize - (3 * textLength / 2) * fontSize
+        startY += 4 * self.__squareSize / 3
+        turtle.goto(startY, startX)
+        self.__drawSquare("blue")
+        turtle.goto(startY + textOffsetY, startX - textOffsetX)
+        turtle.write("Jewel")
+
+        # 'Dirt + Jewel' caption
+        textOffsetY = self.__squareSize - 9 * 8 # squareSize - (3 * textLength / 2) * fontSize
+        startY += 4 * self.__squareSize / 3
+        turtle.goto(startY, startX)
+        self.__drawSquare("purple")
+        turtle.goto(startY + textOffsetY, startX - textOffsetX)
+        turtle.write("Dirt + Jewel")
