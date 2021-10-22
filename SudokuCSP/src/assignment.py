6e756ba8b1a836f8a33cb1ec9eb0a5a1976f87c0
koechslin <killian.oechslin@insa-lyon.fr>
2021-10-22 12:22:02 -0400

    fix indent

--- SudokuCSP/src/assignment.py
+++ SudokuCSP/src/assignment.py
@@ -3,7 +3,7 @@ from cell import Cell
 
 class Assignment:
     def __init__(self, sudoku):
-		# Sudoku Matrix of Cells
+        # Sudoku Matrix of Cells
         self.__sudoku
 
     def isConsistant(self):
@@ -12,12 +12,12 @@ class Assignment:
     def isComplete(self):
         return True
 
-	def getNextUnassignedCell(self):
+    def getNextUnassignedCell(self):
         return Cell
 
     def getSudoku(self):
         return self.__sudoku
 
     def __str__(self):
-		# return sudoku in a string
+        # return sudoku in a string
         return "__sudoku :\n"
--- SudokuCSP/src/cell.py
+++ SudokuCSP/src/cell.py
@@ -1,27 +1,29 @@
 
 class Cell:
     def __init__(self, sudoku):
-		# Value in the cell (int from 1 to 9)
+        # Value in the cell (int from 1 to 9)
         self.__value
-		# List of possible values for this cell
-		self.__possibleValues
+        # List of possible values for this cell
+        self.__possibleValues
 
     def addPossibleValue(self, value):
+        pass
 
     def removePossibleValue(self, value):
+        pass
 
-	def hasValue(self):
+    def hasValue(self):
         return True
 
-	def getPossibleValues(self)
-		return self.__possibleValues
+    def getPossibleValues(self):
+        return self.__possibleValues
 
     def getValue(self):
         return self.__value
 
-	def setValue(self, value)
-		self.__value = value
+    def setValue(self, value):
+        self.__value = value
 
     def __str__(self):
-		# return Cell value in a string
+        # return Cell value in a string
         return str(self.__value)
